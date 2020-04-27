11cdb162c9b0324fd3ebd7af98005bdf29a52432
mahimaagrawal2510 <62229482+mahimaagrawal2510@users.noreply.github.com>
2020-04-27 02:33:30 -0700

    Create array.basic.cpp

---
+++ array.basic.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+  int a[10]={0};
+  //size of
+  cout<< sizeof(a) / sizeof(int);
+  cout<<n<<endl;
+  for (int i=0;i<5;i++){
+    cin>>a[i];
+    
+  }
+  for (int i=0;i<5;i++){
+    cout<<a[i]<<",";
+    
+  }
+  return 0;
+}
